fef59a961d5721bf0e4cc546696b0cdff28a70cd
Marcin Budny <marcin.budny@gmail.com>
2015-11-29 22:16:32 +0100

    added restore opeartion id middleware

---
+++ src/ApplicationInsights.OwinExtensions.Tests/RestoreOperationIdContextMiddlewareTests.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Threading.Tasks;
+using ApplicationInsights.OwinExtensions.Tests.Utils;
+using FluentAssertions;
+using Xunit;
+
+namespace ApplicationInsights.OwinExtensions.Tests
+{
+    public class RestoreOperationIdContextMiddlewareTests : IDisposable
+    {
+        public RestoreOperationIdContextMiddlewareTests()
+        {
+            OperationIdContext.Clear();
+        }
+
+        [Fact]
+        public async Task Can_Restore_Operation_Context_Id_From_Owin_Context()
+        {
+            var context = new MockOwinContext();
+            context.Set("OperationIdContext", "test");
+
+            var collector = new OperationIdCollectingMiddleware();
+            var sut = new RestoreOperationIdContextMiddleware(collector);
+
+            await sut.Invoke(context);
+
+            collector.OperationIdFromAmbientContext.Should().Be("test");
+        }
+
+        [Fact]
+        public async Task Should_Not_Substitue_Existing_Operation_Id_If_Context_Contains_Null()
+        {
+            OperationIdContext.Set("test");
+            var context = new MockOwinContext();
+
+            var collector = new OperationIdCollectingMiddleware();
+            var sut = new RestoreOperationIdContextMiddleware(collector);
+
+            await sut.Invoke(context);
+
+            collector.OperationIdFromAmbientContext.Should().Be("test");
+        }
+
+        public void Dispose()
+        {
+            OperationIdContext.Clear();
+        }
+    }
+}
---
+++ src/ApplicationInsights.OwinExtensions/RestoreOperationIdContextMiddleware.cs
@@ -0,0 +1,21 @@
+﻿using System.Threading.Tasks;
+using Microsoft.Owin;
+
+namespace ApplicationInsights.OwinExtensions
+{
+    public class RestoreOperationIdContextMiddleware : OwinMiddleware
+    {
+        public RestoreOperationIdContextMiddleware(OwinMiddleware next) : base(next)
+        {
+        }
+
+        public override async Task Invoke(IOwinContext context)
+        {
+            var operationId = context.Get<string>("OperationIdContext");
+            if(operationId != null)
+                OperationIdContext.Set(operationId);
+
+            await Next.Invoke(context);
+        }
+    }
+}
