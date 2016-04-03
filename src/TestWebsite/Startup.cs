e55f1725758c0a9b7cb6d581c5478fee02fdcfc2
Marcin Budny <marcin.budny@gmail.com>
2016-04-03 18:26:06 +0200

    included app insights into test website

--- src/TestWebsite/Startup.cs
+++ src/TestWebsite/Startup.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Web.Http;
+using ApplicationInsights.OwinExtensions;
 using Microsoft.Owin;
+using Microsoft.Owin.Security.OAuth;
 using Owin;
 
 [assembly: OwinStartup(typeof(TestWebsite.Startup))]
@@ -11,10 +13,38 @@ namespace TestWebsite
     {
         public void Configuration(IAppBuilder appBuilder)
         {
+            appBuilder.UseApplicationInsights();
+
+            ConfigureAuth(appBuilder);
+
+            appBuilder.RestoreOperationIdContext();
+
+            ConfigureWebApi(appBuilder);
+        }
+
+        private static void ConfigureAuth(IAppBuilder appBuilder)
+        {
+            var options = new OAuthAuthorizationServerOptions
+            {
+                TokenEndpointPath = new PathString("/Token"),
+                Provider = new OAuthAuthorizationServerProvider(),
+                AuthorizeEndpointPath = new PathString("/api/Account/ExternalLogin"),
+                AccessTokenExpireTimeSpan = TimeSpan.FromDays(14),
+                AllowInsecureHttp = true
+            };
+
+            appBuilder.UseOAuthAuthorizationServer(options);
+
+            appBuilder.UseOAuthBearerAuthentication(new OAuthBearerAuthenticationOptions());
+        }
+
+        private static void ConfigureWebApi(IAppBuilder appBuilder)
+        {
             var config = new HttpConfiguration();
             config.MapHttpAttributeRoutes();
 
             appBuilder.UseWebApi(config);
         }
     }
+
 }
