5207b92dfe5044801875b8b777ae628b834d7249
Marcin Budny <marcin.budny@gmail.com>
2016-04-03 16:35:58 +0200

    created a test case

--- src/TestWebsite/Controllers/ValuesController.cs
+++ src/TestWebsite/Controllers/ValuesController.cs
@@ -1,8 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net;
+﻿using System.Collections.Generic;
 using System.Net.Http;
+using System.Threading.Tasks;
 using System.Web.Http;
 
 namespace TestWebsite.Controllers
@@ -10,32 +8,20 @@ namespace TestWebsite.Controllers
     [RoutePrefix("api/values")]
     public class ValuesController : ApiController
     {
-        // GET api/values
         [Route("")]
-        public IEnumerable<string> Get()
+        public async Task<IEnumerable<string>> Get()
         {
-            return new string[] { "value1", "value2" };
+            using (var client = new HttpClient())
+            {
+                await client.GetAsync("https://google.com");
+
+                await client.GetAsync("https://bing.com");
+
+                await client.GetAsync("https://yahoo.com");
+            }
+
+            return new[] { "value1", "value2" };
         }
 
-        // GET api/values/5
-        public string Get(int id)
-        {
-            return "value";
-        }
-
-        // POST api/values
-        public void Post([FromBody]string value)
-        {
-        }
-
-        // PUT api/values/5
-        public void Put(int id, [FromBody]string value)
-        {
-        }
-
-        // DELETE api/values/5
-        public void Delete(int id)
-        {
-        }
     }
 }
