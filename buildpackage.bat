e0652bb3489f21c289ded457d6ab296453107354
Marcin Budny <marcin.budny@gmail.com>
2017-04-04 22:32:53 +0200

    changed build batch path to msbuild

--- buildpackage.bat
+++ buildpackage.bat
@@ -1,6 +1,6 @@
 gitversion /updateassemblyinfo 
 
-"C:\Program Files (x86)\MSBuild\14.0\Bin\MSBuild.exe" src\ApplicationInsights.OwinExtensions.sln /p:Configuration=Release
+"C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin\MSBuild.exe" src\ApplicationInsights.OwinExtensions.sln /p:Configuration=Release
 
 gitversion /exec _dopackage.bat
 
